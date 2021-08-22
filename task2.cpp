e33000313244708dce935b3a945a74a0a05ee714
stifler33 <bagi33.ru@gmail.com>
2021-08-22 17:00:09 +0300

    task2 finish

--- task2.cpp
+++ task2.cpp
@@ -7,9 +7,15 @@ int main(){
     ifstream buffer;
     while (locationFile != "no"){
         cout << "Enter location file :";
-        cin >> locationFile;
+        //cin >> locationFile;
+        getline(cin,locationFile);
         buffer.open(locationFile, ios::binary);
-        buffer.is_open() ? cout << "File open !\n" : cout << "Error open file\n";
+        if(buffer.is_open()){
+            cout << "File open !\n";
+        } else{
+            cerr << "Error open file\n";
+            continue;
+        }
         while (!buffer.eof()){
             char outputText[100] = {'\0'};
             buffer.read(outputText, 100);
@@ -17,7 +23,8 @@ int main(){
         }
         buffer.close();
         cout << "open another file ?\n";
-        cin >> locationFile;
+        //cin >> locationFile;
+        getline(cin,locationFile);
         if (locationFile == "no") {
             cout << "Goodbye !\n";
             break;
