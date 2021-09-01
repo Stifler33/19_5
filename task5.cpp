201e13af33104173661b23363d7c24d04e490319
stifler33 <bagi33.ru@gmail.com>
2021-09-01 20:59:14 +0300

    task5 work 0.5

--- task5.cpp
+++ task5.cpp
@@ -3,47 +3,61 @@
 #include <bitset>
 #include <vector>
 using namespace std;
+bitset<13> numberQuest;
 bool EndGame(bitset<13> *numberQuest){
-    cout << *numberQuest << endl;
+    //cout << *numberQuest << endl;
     for (int i = 0; i < numberQuest->size(); i++){
         if (numberQuest->test(i) == 0) return false;
     }
     return true;
 }
-void changePoint(int &point, int &step){
-    for (int i = point; step > 0; step--){
-        i++;
-        if (i > 12) i = 0;
-        point = i;
-    }
+void changePoint(int &point, int step){
+    do {
+        for (int i = point; step > 0; step--) {
+            i++;
+            if (i > 12) i = 0;
+            point = i;
+        }
+        if (numberQuest.test(point)) step = 1;
+    }while (numberQuest.test(point));
 }
 int main(){
     ifstream bufQuest;
     string locationQuest;
-    bitset<13> numberQuest;
     int step = 0;
     int point = -1;
-    while(!EndGame(&numberQuest)){
-    cin >> step;
+    int scorePlayer = 0;
+    int scoreViewer = 0;
+    while(!EndGame(&numberQuest) && scorePlayer < 6 && scoreViewer < 6){
+        cout << "Enter step :";
+        cin >> step;
         changePoint(point, step);
-    //"C:\\quest\\Quest.txt";
-    locationQuest = "C:\\quest\\Quest.txt";
-    locationQuest.insert(14, to_string(point + 1));
-    bufQuest.open(locationQuest, ios::binary);
-    string textQuest;
-    getline(bufQuest , textQuest , '\r');
-    bufQuest.seekg((int)bufQuest.tellg() + 1);
-    cout << textQuest << endl;
-    string userAnswer;
-    cin >> userAnswer;
-    getline(bufQuest , textQuest , '\r');
-    if (textQuest == userAnswer){
-        cout << "Good !\n";
-    }else cout << "wrong answer\n";
-        if (!numberQuest.test(point)){
-            numberQuest.set(point);
+        locationQuest = "D:\\game\\quest.txt";
+        locationQuest.insert(13, to_string(point + 1));
+        bufQuest.open(locationQuest, ios::binary);
+        string textQuest;
+        getline(bufQuest , textQuest , '\r');
+        bufQuest.seekg((int)bufQuest.tellg() + 1);
+        cout << textQuest << endl;
+        string userAnswer;
+        cin >> userAnswer;
+        getline(bufQuest , textQuest , '\r');
+        if (textQuest == userAnswer){
+            cout << "Good !\n";
+            scorePlayer++;
+        }else {
+            cout << "wrong answer\n";
+            scoreViewer++;
         }
-        bufQuest.close();
+            if (!numberQuest.test(point)){
+                numberQuest.set(point);
+            }
+            bufQuest.close();
+    }
+    if (scorePlayer >= 6){
+        cout << "Win player\n";
+    }else if (scoreViewer >= 6){
+        cout << "win viewer\n";
     }
     return 0;
 }
